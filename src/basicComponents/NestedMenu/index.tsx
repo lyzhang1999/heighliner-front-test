cc31596e664f18197ee55a56c80112bb2e16c59c
Pandy <wkpcoder@163.com>
2022-06-13 18:07:52 +0800

    Feat: add nested menu

--- src/basicComponents/NestedMenu/index.tsx
+++ src/basicComponents/NestedMenu/index.tsx
@@ -3,32 +3,83 @@
  */
 
 import React, { ReactElement } from "react";
+import {
+  Collapse,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { useRouter } from "next/router";
+import clsx from "clsx";
 
-interface Menu {
+import utils from "@/utils/utils";
+import styles from "./index.module.scss";
+
+export interface Menu {
   icon?: ReactElement;
   title: string;
   to?: string;
-  items: Menu[];
+  items?: Menu[];
 }
 
+export type MenuList = Menu[];
+
 interface Props {
-  menuList: Menu[];
+  menuList: MenuList;
+}
+
+function isActiveNav(currentPath: string) {
+  if (utils.isBrowser()) {
+    return (location.href as string).includes(currentPath);
+  } else {
+    return false;
+  }
 }
 
 export default function NestedMenu({ menuList }: Props): React.ReactElement {
+  const [open, setOpen] = React.useState(false);
+
+  const router = useRouter();
+
+  const expandSubmenu = () => {
+    setOpen(!open);
+  };
+
   return (
-    <>
+    <div className={styles.wrap}>
       {menuList.map((menu) => {
-        // Have multiple sub-items
+        // Have multiple sub-items, recursively invoke NestedMenu component. 
         if (menu.items && menu.items.length > 0) {
           return (
-            <div>
-              <NestedMenu menuList={menu.items} />
-            </div>
+            <>
+              <ListItemButton onClick={expandSubmenu}>
+                <ListItemText primary={menu.title} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <div className={styles.nestedMenu}>
+                  <NestedMenu menuList={menu.items} />
+                </div>
+              </Collapse>
+            </>
           );
         } else {
+          return (
+            <ListItemButton
+              key={menu.title}
+              onClick={() => {
+                router.push(menu.to!);
+              }}
+              className={clsx(isActiveNav(menu.to!) && styles.activeMenu)
+              }
+            >
+              <ListItemText primary={menu.title} />
+            </ListItemButton>
+          );
         }
       })}
-    </>
+    </div>
   );
 }

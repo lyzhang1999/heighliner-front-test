f8c88d10d38b66e5d9f1048de8b5fc571f6c2eac
Pandy <wkpcoder@163.com>
2022-06-16 17:09:43 +0800

    Feat: application panel

---
+++ src/utils/CDN.ts
@@ -0,0 +1,9 @@
+/**
+ * All resources store in the CDN
+ */
+
+export const GinIcon = "https://assets-1309519128.cos.ap-hongkong.myqcloud.com/Gin%403x.webp";
+export const NextIcon = "https://assets-1309519128.cos.ap-hongkong.myqcloud.com/Nextjs%403x.webp";
+export const SpringIcon = "https://assets-1309519128.cos.ap-hongkong.myqcloud.com/Spring%403x.webp";
+export const VueIcon = "https://assets-1309519128.cos.ap-hongkong.myqcloud.com/Vue%403x.webp";
+export const RemixIcon = "https://assets-1309519128.cos.ap-hongkong.myqcloud.com/Remix%403x.webp";

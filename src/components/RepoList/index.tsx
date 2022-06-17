3d3f8a1a34ce131a208e40d6ff6fbe7fd969e37f
zhangze <1183460943@qq.com>
2022-06-17 15:04:47 +0800

    feat: add repos

--- src/components/RepoList/index.tsx
+++ src/components/RepoList/index.tsx
@@ -4,8 +4,20 @@ const list = [
   {
     repoName: 'my-shop-backend',
     tags: ['v0.1.2', 'v0.1.0'],
-    PRS: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
-    Commits: [{
+    prs: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
+    commits: [{
+      commitID: 'fdasdf',
+      commitMessage: 'fix: fix ... '
+    },{
+        commitID: 'fdafdsasdf',
+        commitMessage: 'fix: .fewqwfq.. '
+      }]
+  },
+  {
+    repoName: 'my-shop-backend',
+    tags: ['v0.1.2', 'v0.1.0'],
+    prs: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
+    commits: [{
       commitID: 'fdasdf',
       commitMessage: 'fix: fix ... '
     }]
@@ -13,17 +25,8 @@ const list = [
   {
     repoName: 'my-shop-backend',
     tags: ['v0.1.2', 'v0.1.0'],
-    PRS: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
-    Commits: [{
-      commitID: 'fdasdf',
-      commitMessage: 'fix: fix ... '
-    }]
-  },
-  {
-    repoName: 'my-shop-backend',
-    tags: ['v0.1.2', 'v0.1.0'],
-    PRS: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
-    Commits: [{
+    prs: ['h#269 refactor: push code to github', '#270 refactor: use base image'],
+    commits: [{
       commitID: 'fdasdf',
       commitMessage: 'fix: fix ... '
     }]
@@ -41,7 +44,47 @@ export default function RepoList() {
                 <span className={styles.name}>
                   {item.repoName}
                 </span>
-                <img src="" alt="" className={styles.icon}/>
+                <img src="/img/application/panel/goRepo.svg" alt="" className={styles.icon}/>
+              </div>
+              <div className={styles.tags}>
+                <img src="/img/gitprovider/github.webp" alt="github" className={styles.github}/>
+                <span className={styles.line}>|</span>
+                <span className={styles.tag}>tags</span>
+                {
+                  item.tags.map(i => {
+                    return (
+                      <span>
+                          <span className={styles.line}>|</span>
+                          <span className={styles.tag}>{i}</span>
+                      </span>
+                    )
+                  })
+                }
+              </div>
+              <div className={styles.lineCenter}></div>
+
+              <div className={styles.prs}>
+                <div className={styles.left}>
+                  PRS:
+                </div>
+                <div className={styles.right}>
+                  {item.prs.map((i, index) => {
+                    return (<div className={styles.prsItem} key={index}>{i}</div>)
+                  })}
+                </div>
+              </div>
+              <div className={styles.prs}>
+                <div className={styles.left}>
+                  Commits:
+                </div>
+                <div className={styles.right}>
+                  {item.commits.map((i, index) => {
+                    return (
+                      <div className={styles.prsItem} key={index}>
+                        <span>{i.commitID}</span> <span className={styles.commitMessage}>({i.commitMessage})</span>
+                      </div>)
+                  })}
+                </div>
               </div>
             </div>
           )

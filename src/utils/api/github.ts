52635ffe2b85b6238ace95132b89c5b322499cc7
Pandy <wkpcoder@163.com>
2022-06-24 14:27:45 +0800

    Hotfix: add comment for putInstallation. (#4)
    * Fix: restructure comment for putInstallation comment

--- src/utils/api/github.ts
+++ src/utils/api/github.ts
@@ -6,6 +6,10 @@ export interface PutInstallationReq {
   installation_id: string;
 }
 
+/**
+ *  After user installed GitHub app, GitHub will return an installation_id 
+ *  which will be sent to backend service to do further authorization.
+ */
 export function putInstallation(req: PutInstallationReq) {
   return http.put(
     `/orgs/${req.org_id}/github/installation/${req.installation_id}`

56168f875a5b5559bf2ddf82cb351d04d9049a20
zhangze <1183460943@qq.com>
2022-06-24 09:41:12 +0800

    feat: add application list hover

--- src/components/ApplicationList/index.tsx
+++ src/components/ApplicationList/index.tsx
@@ -34,50 +34,52 @@ export default function ApplicationList({list, clusterList}: Props) {
             cluster = get(cluster, 'name');
           }
           return (
-            <div className={styles.item} onClick={() => goPanel(item.app_id, item.last_release.id, status)}
-                 key={item.app_id}>
-              <div className={styles.left}>
-                {
-                  statckIcon && <img src={statckIcon} alt=""/>
-                }
-              </div>
-              <div className={styles.right}>
-                <div className={styles.title}>
-                  <span>{item.app_name}</span>
+            <div className={styles.itemWrapper} key={item.app_id}>
+              <div className={styles.item} onClick={() => goPanel(item.app_id, item.last_release.id, status)}
+                   key={item.app_id}>
+                <div className={styles.left}>
                   {
-                    (status === ApplicationStatus.COMPLETED) &&
-                    <span className={styles.statusIcon}>
+                    statckIcon && <img src={statckIcon} alt=""/>
+                  }
+                </div>
+                <div className={styles.right}>
+                  <div className={styles.title}>
+                    <span>{item.app_name}</span>
+                    {
+                      (status === ApplicationStatus.COMPLETED) &&
+                      <span className={styles.statusIcon}>
                       <img src="/img/application/create.webp" alt=""/>
                       <span className={styles.running}>running</span>
                     </span>
-                  }
-                  {
-                    (status === ApplicationStatus.PROCESSING) &&
-                    <span className={styles.statusIcon}>
+                    }
+                    {
+                      (status === ApplicationStatus.PROCESSING) &&
+                      <span className={styles.statusIcon}>
                       <img src="/img/application/creating.webp" alt="" className={styles.rotate}/>
                       <span className={styles.creating}>Creating</span>
                     </span>
-                  }
-                  {
-                    (status === ApplicationStatus.FAILED) &&
-                    <span className={styles.statusIcon}>
+                    }
+                    {
+                      (status === ApplicationStatus.FAILED) &&
+                      <span className={styles.statusIcon}>
                       <img src="/img/application/createfailed.webp" alt=""/>
                       <span className={styles.failed}>Failed</span>
                     </span>
-                  }
-                </div>
-                <div className={styles.status}>
-                  Cluster： {cluster && cluster}
-                </div>
-                <div className={styles.status}>
-                  CreatTime： {formatDate(get(item, ['last_release', 'created_at']) * 1000)}
-                </div>
-                {
-                  get(item, ['stack', 'name']) &&
-                  <span className={styles.stack}>
+                    }
+                  </div>
+                  <div className={styles.status}>
+                    Cluster： {cluster && cluster}
+                  </div>
+                  <div className={styles.status}>
+                    CreatTime： {formatDate(get(item, ['last_release', 'created_at']) * 1000)}
+                  </div>
+                  {
+                    get(item, ['stack', 'name']) &&
+                    <span className={styles.stack}>
                     {get(item, ['stack', 'name'])}
                   </span>
-                }
+                  }
+                </div>
               </div>
             </div>
           )

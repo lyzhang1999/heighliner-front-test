dd8518f4315bf565ae81fcee5d3c0ea86cf0214f
zhangze <1183460943@qq.com>
2022-06-28 14:12:44 +0800

    pref: add disable github

--- src/components/AddGitProvider/GitHubApp/index.tsx
+++ src/components/AddGitProvider/GitHubApp/index.tsx
@@ -77,7 +77,7 @@ export default function GitHubApp(props: Props): React.ReactElement {
         justifyContent="center"
         alignItems="center"
       >
-        <Button onClick={openGitHubAppInstallationWindow} variant="outlined">
+        <Button onClick={openGitHubAppInstallationWindow} variant="outlined" disabled>
           <AddCircleOutlineIcon />
           &nbsp;Add a new organization
         </Button>

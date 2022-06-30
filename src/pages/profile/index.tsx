7234d5258d0fa49b637ddf1e0fa5810b0eae8c87
Pandy <wkpcoder@163.com>
2022-06-30 16:16:04 +0800

    Feat: migrate logout functionality to profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Container, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 import Layout from "@/components/Layout";
 import BasicProfile from "@/components/setting/profile/BasicProfile";
 import Password from "@/components/setting/profile/Password";
+import Email from "@/components/setting/profile/Email";
 
 import styles from "./index.module.scss";
-import Email from "@/components/setting/profile/Email";
+import Logout from "@/components/setting/profile/Logout";
 
 export default function Setting(): React.ReactElement {
   return (
@@ -24,6 +25,9 @@ export default function Setting(): React.ReactElement {
             {/*</div>*/}
             <div className={styles.card}>
               <Password />
+            </div>
+            <div className={styles.card}>
+              <Logout />
             </div>
           </Stack>
         </div>

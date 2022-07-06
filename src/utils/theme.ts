e09c5e480cdcbc9535d26efa61629486cbb0e329
Pandy <wkpcoder@163.com>
2022-07-06 11:24:00 +0800

    Fix: table and header change font-family

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -100,6 +100,13 @@ const theme = createTheme({
         },
       },
     },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          fontFamily: "Inter"
+        }
+      }
+    },
   },
 });
 

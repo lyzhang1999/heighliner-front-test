2f12f9df31b8f9c48014170e816b015c171b3154
zhangze <1183460943@qq.com>
2022-07-22 16:58:29 +0800

    feat: add deployname hendleclick

--- src/components/Panel/EnvList/index.tsx
+++ src/components/Panel/EnvList/index.tsx
@@ -2,6 +2,7 @@ import styles from './index.module.scss';
 import {Fragment, useState} from "react";
 import clsx from "clsx";
 import {EnvList as IEnvList} from "@/api/application";
+import {get} from "lodash-es";
 
 const item =
   {
@@ -56,9 +57,9 @@ export default function EnvList({spreadCb, envlist}: Props) {
                 {/*     onClick={() => spread(index)}/>*/}
                 <div className={styles.header}>
                   <div className={styles.envName}>{i.name}</div>
-                  <div className={styles.iconWrapper}>
-                    <img src="/img/cluster/aws.webp" alt=""/>
-                  </div>
+                  {/*<div className={styles.iconWrapper}>*/}
+                  {/*  <img src="/img/cluster/aws.webp" alt=""/>*/}
+                  {/*</div>*/}
                 </div>
                 <div className={styles.urlTitle}>
                   PUBLIC URL:
@@ -71,19 +72,27 @@ export default function EnvList({spreadCb, envlist}: Props) {
                   <span className={styles.key}>
                       Status:
                   </span>
-                  <span className={styles.value}>
+                    <span className={styles.value}>
                     <span className={styles.circle}></span>
                     <span className={styles.statusValue}>Running</span>
                   </span>
                   </div>
-                  {/*<div className={styles.configWrapper}>*/}
-                  {/* <span className={styles.key}>*/}
-                  {/*    Configs:*/}
-                  {/* </span>*/}
-                  {/*  <span className={styles.configValue}>*/}
-                  {/*  main/values.yaml*/}
-                  {/*</span>*/}
-                  {/*</div>*/}
+                  <div className={styles.configWrapper}>
+                   <span className={styles.key}>
+                      Configs:
+                   </span>
+                    <span className={styles.configValue} onClick={() => {
+                      let url = get(i, 'setting.application.deploy.url', '');
+                      let valueName = get(i, 'setting.application.deploy.values_file', '');
+                      let name = i.name;
+                      let path = get(i, 'setting.application.deploy.path', '');
+                      window.open(`${url}/tree/${name}/${path}/${valueName}`)
+                    }}>
+                      {
+                        get(i, 'setting.application.deploy.values_file', '')
+                      }
+                  </span>
+                  </div>
                   <div className={styles.logsWrapper}>
                    <span className={styles.key}>
                       Logs:

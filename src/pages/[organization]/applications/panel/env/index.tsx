13f84b5730dcf7f5d3284d12ff340bdb5c0dfec3
Pandy <wkpcoder@163.com>
2022-07-28 16:15:05 +0800

    Feat: add kubeconfig into EnvContext (#47)

--- src/pages/[organization]/applications/panel/env/index.tsx
+++ src/pages/[organization]/applications/panel/env/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 import Layout from "@/components/Layout";
 import RepoList from "@/components/Panel/RepoList";
@@ -10,6 +10,9 @@ import Resources from "@/components/Panel/Env/Resources";
 
 import styles from "./index.module.scss";
 import { ResourceType } from "@/api/application";
+import useApplication from "@/hooks/application";
+import { getQuery } from "@/utils/utils";
+import { getCluster } from "@/api/cluster";
 
 const tabItems: Array<{
   label: ResourceType;
@@ -34,24 +37,51 @@ const tabItems: Array<{
   },
 ];
 
+interface IEnvContext {
+  cluster_id?: number;
+  kubeconfig?: string;
+}
+
+export const EnvContext = createContext<IEnvContext>({});
+
 export default function Env(): React.ReactElement {
   const [selectedTab, setSelectedTab] = useState(tabItems[0].label);
+  const [envContext, setEnvContext] = useState<IEnvContext>({});
+
+  // Get the kubeconfig
+  const app_id = +getQuery("app_id");
+  const [application] = useApplication({ app_id });
+  useEffect(() => {
+    if (application && application.cluster_id > 0) {
+      getCluster({
+        cluster_id: application.cluster_id,
+      }).then((res) => {
+        setEnvContext((preState) => ({
+          ...preState,
+          cluster_id: application.cluster_id,
+          kubeconfig: res.kubeconfig,
+        }));
+      });
+    }
+  }, [application]);
 
   return (
     <Layout notStandardLayout pageHeader="applications/my shop">
-      <div className={styles.wrapper}>
-        <div className={styles.main}>
-          <Main />
-          <SlideTabs {...{ tabItems, selectedTab, setSelectedTab }} />
-          {/* <Projects /> */}
-          <Resources
-            {...{
-              selectedResourceType: selectedTab as ResourceType,
-            }}
-          />
+      <EnvContext.Provider value={envContext}>
+        <div className={styles.wrapper}>
+          <div className={styles.main}>
+            <Main />
+            <SlideTabs {...{ tabItems, selectedTab, setSelectedTab }} />
+            {/* <Projects /> */}
+            <Resources
+              {...{
+                selectedResourceType: selectedTab as ResourceType,
+              }}
+            />
+          </div>
+          {/* <RepoList /> */}
         </div>
-        {/* <RepoList /> */}
-      </div>
+      </EnvContext.Provider>
     </Layout>
   );
 }
